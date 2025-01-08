b5a1c2e26b0a8598f57c8180b07e5606c498aaef
Braden Simpson <bradensimpsons@gmail.com>
2025-01-08 10:12:38 -0800

    feat: use github copilot as it has gpt 4 baked in fo free

--- lua/plugins/gp.lua
+++ lua/plugins/gp.lua
@@ -4,9 +4,28 @@ return {
   config = function()
     local conf = {
       -- For customization, refer to Install > Configuration in the Documentation/Readme
+      providers = {
+
+        openai = {
+          disable = true,
+          endpoint = "https://api.openai.com/v1/chat/completions",
+          secret = os.getenv("OPENAI_API_KEY"),
+        },
+        copilot = {
+          endpoint = "https://api.githubcopilot.com/chat/completions",
+          secret = {
+            "bash",
+            "-c",
+            "cat ~/.config/github-copilot/hosts.json | sed -e 's/.*oauth_token...//;s/\".*//'",
+          },
+        },
+      },
+      -- anthropic = {
+      --   endpoint = "https://api.anthropic.com/v1/messages",
+      --   secret = os.getenv("ANTHROPIC_API_KEY"),
+      -- },
     }
     require("gp").setup(conf)
-
     -- Setup shortcuts here (see Usage > Shortcuts in the Documentation/Readme)
   end,
 }
